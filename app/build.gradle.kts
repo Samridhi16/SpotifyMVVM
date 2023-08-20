fc35e7077500d00e78e8c6db9d938f2fe55061e4
Samridhi16 <samridhipramanik@gmail.com>
2023-08-19 23:51:00 -0700

    firebase connected

---
+++ app/build.gradle.kts
@@ -0,0 +1,107 @@
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
+//    id 'com.google.gms.google-services'
+}
+
+android {
+    namespace = "com.sam.spotifymvvm"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.sam.spotifymvvm"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Material Design
+    implementation("com.google.android.material:material:1.11.0-alpha02")
+
+    // Architectural Components
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0-alpha01")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-alpha01")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // Coroutine Lifecycle Scopes
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-alpha01")
+
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.7.0")
+    kapt ("com.github.bumptech.glide:compiler:4.15.1")
+
+    // Activity KTX for viewModels()
+    implementation("androidx.activity:activity-ktx:1.8.0-alpha06")
+
+    //Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
+    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt ("androidx.hilt:hilt-compiler:1.1.0-alpha01")
+
+
+    // Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    // Firebase Firestore
+    implementation("com.google.firebase:firebase-firestore:24.7.0")
+
+    // Firebase Storage KTX
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+
+    // Firebase Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+
+    // ExoPlayer
+    implementation("com.google.android.exoplayer:exoplayer-core:2.19.1")
+    implementation("com.google.android.exoplayer:exoplayer-ui:2.19.1")
+    implementation("com.google.android.exoplayer:extension-mediasession:2.19.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
